20d5d6116d9eeebfc2c0acb606b35aa57d2c1718
NataLy <nikifor1114@mail.ru>
2021-09-28 19:32:58 +0700

    Add condition IF

--- app.rb
+++ app.rb
@@ -1,2 +1,10 @@
 puts "What site are we going to parse? (K)arus.ru, (R)arim.ru"
 str = gets.strip.capitalize
+
+if str == 'K'
+  puts 'K'
+elsif str == 'R'
+  puts 'R'
+else
+  puts "You haven't selected anything"
+end
