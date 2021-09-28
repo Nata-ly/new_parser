6a5fbed12ce990c620c017858a13c568aa222b8a
NataLy <nikifor1114@mail.ru>
2021-09-28 20:07:10 +0700

    Add https request for karus_method

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
+require 'nokogiri'
+require 'rest-client'
+
+def karus_method
+  server = "https://karus.ru/program/?city=&adv_type=&search=&letters=&page=&per_page=&preview=true&map=false&per_page=True"
+  response = RestClient.get(server)
+  document = Nokogiri::HTML(response)
+  table = document.at('table.table-data')
+end
+
 puts "What site are we going to parse? (K)arus.ru, (R)arim.ru"
 str = gets.strip.capitalize
 
 if str == 'K'
-  puts 'K'
+  karus_method
 elsif str == 'R'
   puts 'R'
 else
